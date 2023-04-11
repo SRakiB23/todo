e164857a788afdab4f6cf6e666198001f4a163f1
SRakiB23 <128783359+SRakiB23@users.noreply.github.com>
2023-04-11 15:55:16 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # todo
-A mobile application to write regular task in order to maintain  the flow of work. 
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\sazza\\Downloads\\flutter_windows_3.7.8-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Flutter\\todo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\sazza\\Downloads\\flutter_windows_3.7.8-stable\\flutter"
+  "PROJECT_DIR=E:\\Flutter\\todo"
+  "FLUTTER_ROOT=C:\\Users\\sazza\\Downloads\\flutter_windows_3.7.8-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Flutter\\todo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Flutter\\todo"
+  "FLUTTER_TARGET=E:\\Flutter\\todo\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Flutter\\todo\\.dart_tool\\package_config.json"
+)
